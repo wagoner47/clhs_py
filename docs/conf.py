ce0e5c271c6b92791ee66234a2a20286ba615ff2
Erika Wagoner <wagoner47@email.arizona.edu>
2019-10-29 09:00:56 -0700

    Add autosummary extension and set autodoc and autosummary options

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,8 @@ master_doc = 'index'
 extensions = [
     'nbsphinx',
     'sphinx.ext.autodoc',
-    'sphinx.ext.mathjax'
+    'sphinx.ext.mathjax',
+    'sphinx.ext.autosummary'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -80,3 +81,8 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- Autodoc options ---------------------------------------------------------
+autodoc_default_flags = ['show-inheritance']
+autodoc_member_order = 'bysource'
+autosummary_generate = True
